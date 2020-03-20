ad8f55bb831d6b61841c4439f540fd7113af5b52
CaoKao <bksaechao@gmail.com>
2020-03-19 23:17:32 -0700

    added anime character images, created title/card/wrapper components, updated character.json data, mapped over character data and displayed each character in a bootstrap card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Card";
+import Title from "./components/Title";
+import Wrapper from "./components/Wrapper";
+import characters from "./characters.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    characters
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Character List</Title>
+        {this.state.characters.map(character => (
+          <Card
+            removeFriend={this.removeFriend}
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
