70844ef78a02c5237386cfc504da47199b9131d2
CaoKao <bksaechao@gmail.com>
2020-03-20 03:20:53 -0700

    shuffle code working

--- src/App.js
+++ src/App.js
@@ -6,24 +6,73 @@ import Jumbotron from "./components/Jumbotron";
 import Navbar from "./components/Navbar";
 import characters from "./characters.json";
 
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
+
+
 class App extends Component {
 
   state = {
-    characters
+    characters,
+    score: 0,
+    topScore: 0,
+    status: "",
+    clicked: []
+  };
+
+  handleShuffle = () => {
+    let shuffleCharacters = shuffle(characters);
+    this.setState({ characters: shuffleCharacters });
+  };
+
+  handleNewGame = () => {
+    this.setState({
+      clicked: [],
+      score: 0,
+      topScore: this.state.topScore
+    });
+    this.handleShuffle();
+  };
+
+  // clickEvent = () => {
+  //   console.log('This is a click event');
+  // }
+
+  handleClicked = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      // this.handlePoints();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleNewGame();
+    }
   };
 
   render() {
     return <>
-      <Navbar />
+      <Navbar
+        status={this.state.status}
+        score={this.state.score}
+        topScore={this.state.topScore}
+      />
       <Jumbotron />
       <Wrapper>
         {this.state.characters.map(character => (
           <Card
-            // removeFriend={this.removeFriend}
+            // clickEvent={this.clickEvent}
+            handleShuffle={this.handleShuffle}
             id={character.id}
             key={character.id}
             name={character.name}
             image={character.image}
+            handleClicked={this.handleClicked}
+            // handlePoints={this.handlePoints}
+            handleNewGame={this.handleNewGame}
           />
         ))}
       </Wrapper>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./style.css";
 
-function Navbar() {
+function Navbar(props) {
     return <>
     <nav className="navbar position-fixed">
   <a href="#" className="navbar-brand">
     <img id="sharingan" src="/images/sharingan.jpg"></img>
   </a>
   <ul>
-    <li>User Guess</li>
-    <li>Score: | Top Score: </li>
+    <li>{props.status}</li>
+    <li>Score: {props.score}| Top Score: {props.topScore}</li>
   </ul>
 </nav>
     </>
